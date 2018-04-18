2ffeb12c22c114898fc0be55d3a55f3b73b1c3c9
Louie Janbro <louie.janbro@itggot.se>
2018-04-18 13:51:09 +0200

    between

---
+++ lib/between.rb
@@ -0,0 +1,9 @@
+def between(num1,num2,num3)
+    if num2 <= num1 && num1 <= num3
+        return true
+    elsif num3 <= num1 && num1 <= num2
+        return true
+    else
+        return false
+    end
+end
