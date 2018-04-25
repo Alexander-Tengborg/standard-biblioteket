8573842f21fdfc4f775469930dc667436fb4ce5c
Louie Janbro <louie.janbro@itggot.se>
2018-04-25 14:01:04 +0200

    dokumenterat is_even

--- lib/is_even.rb
+++ lib/is_even.rb
@@ -1,3 +1,14 @@
+# num - The Integer to determine wether it is even or not.
+#
+# Examples
+#
+#   is_even(3)
+#   # => false
+#
+#   is_even(-50)
+#   # => true
+#
+# Returns true or false.
 def is_even(num)
     if num % 2 == 0
         return true
