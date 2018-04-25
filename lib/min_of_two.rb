89f9a03e818a0fb4445f8d31364900fc1fbb330b
Louie Janbro <louie.janbro@itggot.se>
2018-04-25 14:29:03 +0200

    dokumenterade min_of_two

--- lib/min_of_two.rb
+++ lib/min_of_two.rb
@@ -1,3 +1,13 @@
+# num1 - One of the Integers to determine which is smaller.
+# num2 - One of the Integers to determine which is smaller.
+
+#
+# Examples
+#
+#   min_of_two(3, 5)
+#   # => 3
+#
+# Returns the smallest integer.
 def min_of_two(num1, num2)
     if num1 < num2
         return num1
