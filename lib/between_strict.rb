f9a110a74cffd7a4eb3cb9824eabdda465589f78
Louie Janbro <louie.janbro@itggot.se>
2018-04-25 14:26:23 +0200

    dokumenterade between_strict

--- lib/between_strict.rb
+++ lib/between_strict.rb
@@ -1,3 +1,13 @@
+# num1 - The Integer to determine wether it is between.
+# num2 - The limit of the range that num1 could be between.
+# num3 - The limit of the range that num1 could be between.
+#
+# Examples
+#
+#   between_strict(2, 1, 10)
+#   # => true
+#
+# Returns true or false.
 def between_strict(num1,num2,num3)
     if num2 < num1 && num1 < num3
         return true
