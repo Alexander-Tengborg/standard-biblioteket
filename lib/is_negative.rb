05b5de0c7933e57d6ae6614acadf004143383bae
Louie Janbro <louie.janbro@itggot.se>
2018-04-25 14:00:52 +0200

    dokumenterat is_negative

--- lib/is_negative.rb
+++ lib/is_negative.rb
@@ -1,3 +1,14 @@
+# num - The Integer to determine wether it is negative or not.
+#
+# Examples
+#
+#   is_negative(3)
+#   # => false
+#
+#   is_negative(-50)
+#   # => true
+#
+# Returns true or false.
 def is_negative(num)
     if num < 0
         return true
