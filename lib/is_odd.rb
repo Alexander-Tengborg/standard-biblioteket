cd21d270f8a6cdbd475fcc037ea69db209cf43cb
Louie Janbro <louie.janbro@itggot.se>
2018-04-25 14:01:16 +0200

    dokumenterat is_odd

--- lib/is_odd.rb
+++ lib/is_odd.rb
@@ -1,3 +1,14 @@
+# num - The Integer to determine wether it is odd or not.
+#
+# Examples
+#
+#   is_odd(3)
+#   # => true
+#
+#   is_odd(-50)
+#   # => false
+#
+# Returns true or false.
 def is_odd(num)
     if num % 2 == 0
         return false
